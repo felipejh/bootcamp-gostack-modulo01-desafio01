98b7976a11b8568809db65a87b97714ea71f4ddf
Felipe Hoffmann <felipehoffmann@MacBook-Pro-de-Felipe.local>
2020-01-29 21:16:14 -0300

    Substituição dos forEach

--- index.js
+++ index.js
@@ -8,89 +8,19 @@ server.listen(3000);
 
 const arrayProjects = [];
 
-// Middleware para mostrar quantas vezes foram feitas requisições
-let qtdReq = 1;
+// Middleware global para mostrar quantas vezes foram feitas requisições
 server.use((req, res, next) => {
   console.time('Request')
   console.log(`Método: ${req.method}; URL: ${req.url}`)
-  console.log(`Quantidade de requisições: ${qtdReq}`)
 
-  qtdReq++;
+  console.count('Quantidade de requisições')
 
   next();
 
   console.timeEnd('Request')
-})
-
-server.post('/projects', (req, res) => {
-
-  const project = req.body;
-
-  arrayProjects.push(project);
-
-  return res.send('Incluído com sucesso');
-
 });
 
-server.get('/projects', (req, res) => {
-
-  return res.json(arrayProjects);
-
-});
-
-server.put('/projects/:id', checkTitleRequired, checkIdInArray, (req, res) => {
-
-  arrayProjects.forEach((element, index) => {
-
-    const { id } = element;
-    const { title } = req.body;
-
-    if (id == req.params.id) {
-      const project = arrayProjects[index]
-      project.title = title;
-
-      arrayProjects[index] = project
-    }
-
-  })
-  return res.json(arrayProjects);
-
-});
-
-server.delete('/projects/:id', checkIdInArray, (req, res) => {
-
-  arrayProjects.forEach((element, index) => {
-    const { id } = element;
-
-    if (id == req.params.id) {
-      arrayProjects.splice(index, 1);
-    }
-
-  })
-
-  return res.json(arrayProjects);
-
-});
-
-server.post('/projects/:id/tasks', checkIdInArray, (req, res) => {
-  const { title } = req.body
-
-  arrayProjects.forEach((element, index) => {
-
-    const { id } = element;
-
-    if (id == req.params.id) {
-      const project = arrayProjects[index]
-      project.tasks = title;
-
-      arrayProjects[index] = project
-    }
-
-  })
-  return res.json(arrayProjects);
-
-})
-
+// Middleware para checar se foi passado um título
 function checkTitleRequired(req, res, next) {
   if (!req.body.title) {
     return res.status(400).json({ error: "Title is required" })
@@ -99,22 +29,81 @@ function checkTitleRequired(req, res, next) {
   return next();
 }
 
+// Middleware par chegar se o id passado existe no array
 function checkIdInArray(req, res, next) {
 
-  let indExiste = 0;
-  arrayProjects.forEach((element, index) => {
+  const { id } = req.params;
 
-    const { id } = element;
+  const project = arrayProjects.find(p => p.id == id);
 
-    if (id == req.params.id) {
-      indExiste = 1;
-    }
-
-  })
-
-  if (indExiste === 0) {
+  if (!project) {
     return res.status(400).json({ error: "ID not exists in array." })
   }
 
   return next();
 }
+
+// Adiciona um novo projeto no array
+server.post('/projects', (req, res) => {
+
+  const { id, title } = req.body
+
+  const project = {
+    id,
+    title,
+    tasks: [],
+  }
+
+  arrayProjects.push(project);
+
+  return res.send('Incluído com sucesso');
+
+});
+
+// Lista todos os projetos do array
+server.get('/projects', (req, res) => {
+
+  return res.json(arrayProjects);
+
+});
+
+// Edita o título de um projeto a partir do id passado
+server.put('/projects/:id', checkTitleRequired, checkIdInArray, (req, res) => {
+
+  const { id } = req.params
+  const { title } = req.body;
+
+  const project = arrayProjects.find(p => p.id == id)
+
+  project.title = title;
+
+  return res.json(arrayProjects);
+
+});
+
+// Delete um projeto a partir do id
+server.delete('/projects/:id', checkIdInArray, (req, res) => {
+
+  const { id } = req.params;
+
+  const index = arrayProjects.findIndex(p => p.id == id);
+
+  arrayProjects.splice(index, 1)
+
+  return res.json(arrayProjects);
+
+});
+
+// Inclui uma tarefa ao projeto
+server.post('/projects/:id/tasks', checkIdInArray, (req, res) => {
+
+  const { id } = req.params;
+  const { title } = req.body;
+
+  const project = arrayProjects.find(p => p.id == id)
+
+  project.tasks.push(title);
+
+  return res.json(arrayProjects);
+
+});
